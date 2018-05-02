169d77844c788a971240f7b14e6d8a7466c595c1
Oleh Petrenko <oleh.petrenko1@gmail.com>
2018-05-03 02:00:54 +0300

    Added empty view models.

---
+++ DirectoryTreeExplorer.ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DirectoryTreeExplorer.ViewModel
+{
+    /// <summary>
+    /// Represents the view model for main window.
+    /// </summary>
+    public sealed class MainWindowViewModel : ViewModelBase
+    {
+
+    }
+}
