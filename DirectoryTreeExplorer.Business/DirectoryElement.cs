4a1f7af938d7c886c6d77d77d0497e3a5d0f48ce
Oleh Petrenko <oleh.petrenko1@gmail.com>
2018-05-05 02:37:07 +0300

    Added first version of directory iterator.

---
+++ DirectoryTreeExplorer.Business/DirectoryElement.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DirectoryTreeExplorer.Business
+{
+    /// <summary>
+    /// Directory element information.
+    /// </summary>
+    public sealed class DirectoryElement
+    {
+        public string Name { get; set; }
+
+        public int Level { get; set; }
+    }
+}
---
+++ DirectoryTreeExplorer.Business/DirectoryIterator.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace DirectoryTreeExplorer.Business
+{
+    /// <summary>
+    /// Generates and provides data about found elements in a directory.
+    /// </summary>
+    public sealed class DirectoryIterator
+    {
+        public List<DirectoryElement> FoundData = new List<DirectoryElement>();
+
+
+        public void IterateThroughDirectoryTree(DirectoryInfo root, int level = 0)
+        {
+            AddFoundDirectoryElement(root.Name, level);
+
+            FileInfo[] files = null;
+
+            try
+            {
+                files = root.GetFiles("*.*");
+            }
+            catch (UnauthorizedAccessException)
+            {
+            }
+            catch (DirectoryNotFoundException)
+            {
+            }
+
+            if (files == null)
+                return;
+
+            var subDirs = root.GetDirectories();
+
+            foreach (var dirInfo in subDirs)
+            {
+                IterateThroughDirectoryTree(dirInfo, level + 1);
+            }
+
+            foreach (var file in files)
+            {
+                AddFoundDirectoryElement(file.Name, level + 1);
+            }
+        }
+
+        private void AddFoundDirectoryElement(string name, int level)
+        {
+            FoundData.Add(new DirectoryElement
+            {
+                Name = name,
+                Level = level
+            });
+        }
+    }
+}
